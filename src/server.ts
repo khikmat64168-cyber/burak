e5f615fd392d5f82093ede80d3ea34a74ce23768
matt <khikmat64168@gmail.com>
2026-05-25 05:43:04 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -13,3 +13,14 @@
 // MVC = MODEL VIEW CONTROLLER
 
 //Design pattern : Middleware , Decoratorlar   malum bo'laklarini  butun backend emas balkim strukturalrini  yechishga hizmat qiladi
+
+/////////////////////////////////
+
+//import moment from 'moment'; //  const moment = require('moment') ;
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log('PORT:', process.env.PORT);
+
+console.log('MONGO_URL:', process.env.MONGO_URL);
