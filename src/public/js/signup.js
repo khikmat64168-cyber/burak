53a6049393e83a8b4a3c4c91fb3509b9cb744817
matt <khikmat64168@gmail.com>
2026-06-20 15:03:39 +0900

    feat: 69chi dars validation mantig'i qilindi

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,25 +1,60 @@
 console.log('Signup frontend javascript file');
 
 $(function () {
-  function validateSignupForm() {
-    const memberNick = $('.member-nick').val();
-    const memberPhone = $('.member-phone').val();
-    const memberPassword = $('.member-password').val();
-    const confirmPassword = $('.confirm-password').val();
+  const fileTarget = $('#input-file');
+  let filename;
 
-    if (
-      memberNick === '' ||
-      memberPhone === '' ||
-      memberPassword === '' ||
-      confirmPassword === ''
-    ) {
-      alert('Please insert all required inputs ');
-      return false;
-    }
+  fileTarget.on('change', function () {
+    console.log('change event fired!');
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0];
+      console.log('uploadFile:', uploadFile);
+      const fileType = uploadFile.type;
+      const validImageType = ['image/jpg', 'image/jpeg', 'image/png']; //maim type
+      if (!validImageType.includes(fileType)) {
+        alert('Please , insert only jpeg , jpng ,png  ');
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+          $('.upload-img-frame')
+            .attr('src', URL.createObjectURL(uploadFile))
+            .addClass('Success');
+        }
 
-    if (memberPassword !== confirmPassword) {
-      alert('Password differs , please check ');
-      return false;
+        filename = $(this)[0].files[0].name;
+      }
+
+      $(this).siblings('.upload-name').val(filename);
     }
-  }
+  });
 });
+
+function validateSignupForm() {
+  const memberNick = $('.member-nick').val();
+  const memberPhone = $('.member-phone').val();
+  const memberPassword = $('.member-password').val();
+  const confirmPassword = $('.confirm-password').val();
+
+  if (
+    memberNick === '' ||
+    memberPhone === '' ||
+    memberPassword === '' ||
+    confirmPassword === ''
+  ) {
+    alert('Please insert all required inputs ');
+    return false;
+  }
+
+  if (memberPassword !== confirmPassword) {
+    alert('Password differs , please check ');
+    return false;
+  }
+
+  const memberImage = $('.member-image').get(0).files[0]
+    ? $('.member-image ').get(0).files[0].name
+    : null;
+  if (!memberImage) {
+    alert('Plesae insert restaurant logo');
+    return false;
+  }
+}
